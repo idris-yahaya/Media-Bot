21aa25b9b350826e52011c14f36b5585238fc224
Idris Yahaya <idrisy2237@gmail.com>
2023-11-18 18:51:29 -0500

    http requests testing

--- src/Media Bot.py
+++ src/Media Bot.py
@@ -1,8 +1,81 @@
 import requests
 from dotenv import load_dotenv
 import os
+from datetime import datetime, timezone
 
-def configure():
+load_dotenv()
+NOTION_TOKEN = os.getenv('NOTION_KEY')
+DATABASE_ID = os.getenv('NOTION_PAGE_ID')
+
+headers = {
+    "Authorization": "Bearer " + NOTION_TOKEN,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28",
+}
+
+def create_page(data: dict):
+    create_url = "https://api.notion.com/v1/pages"
+
+    payload = {"parent": {"database_id": DATABASE_ID}, "properties": data}
+
+    res = requests.post(create_url, headers=headers, json=payload)
+    print(res.status_code)
+    return res
+
+def get_pages(num_pages=None):
+    """
+    If num_pages is None, get all pages, otherwise just the defined number.
+    """
+    url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+
+    get_all = num_pages is None
+    page_size = 100 if get_all else num_pages
+
+    payload = {"page_size": page_size}
+    response = requests.post(url, headers=headers, json=payload)
+
+    data = response.json()
+
+    # Comment this out to dump all data to a file
+    # import json
+    # with open('db.json', 'w', encoding='utf8') as f:
+    #    json.dump(data, f, ensure_ascii=False, indent=4)
+
+    results = data["results"]
+    while data["has_more"] and get_all:
+        payload = {"page_size": page_size, "start_cursor": data["next_cursor"]}
+        url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+        response = requests.post(url, json=payload, headers=headers)
+        data = response.json()
+        results.extend(data["results"])
+
+    return results
+
+pages = get_pages()
+
+for page in pages:
+    page_id = page["id"]
+    props = page["properties"]
+    url = props["URL"]["title"][0]["text"]["content"]
+    title = props["Title"]["rich_text"][0]["text"]["content"]
+    published = props["Published"]["date"]["start"]
+    published = datetime.fromisoformat(published)
+
+    print(published)
+
+
+title = "Test Title"
+description = "Test Description"
+published_date = datetime.now().astimezone(timezone.utc).isoformat()
+data = {
+    "URL": {"title": [{"text": {"content": description}}]},
+    "Title": {"rich_text": [{"text": {"content": title}}]},
+    "Published": {"date": {"start": published_date, "end": None}}
+}
+
+create_page(data)
+
+"""def configure():
     load_dotenv()
 
 def main():
@@ -21,4 +94,4 @@ headers = {
 main()
 print("IT WORKED")
 print(os.getenv('NOTION_KEY'))
-print(os.getenv('NOTION_PAGE_ID'))
+print(os.getenv('NOTION_PAGE_ID'))"""
